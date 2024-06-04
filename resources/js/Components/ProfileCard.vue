bd3852a1bf1abdfc34dcc204528c2e50d1d659b9
Kim Harold <pinanonangkim000@gmail.com>
2024-06-04 09:51:47 -0700

    Create & Layout ProfileCard

---
+++ resources/js/Components/ProfileCard.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div class="card w-full shadow-xl p-4">
+        <div class="flex md:flex-row flex-col md:items-start items-center gap-2">
+            <div class="flex flex-col w-max">
+                <div class="avatar">
+                    <div class="w-40 flex flex-col">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                </div>  
+                <div class="bg-green-500 text-white text-center">Active</div>
+            </div>
+            
+            <div class="flex flex-col gap-2 w-full">
+                <div class="bg-base-200 w-full h-max p-2 rounded-lg px-8 text-center md:text-start">
+                    <p class="text-xl font-bold">Juan Dela Cruz </p>
+                </div>
+
+                <div class="flex md:flex-row flex-col uppercase text-sm overflow-x-scroll md:overflow-x-auto">
+                    <table class="w-full">
+                        <tbody>
+                            <tr>
+                                <td class="border px-2 bg-base-200">Id</td>
+                                <td class="border px-2">201900244</td>
+                                <td class="border px-2 bg-base-200">Assigned Office</td>
+                                <td class="border px-2">HUMAN RESOURCE MANAGEMENT AND DEVELOPMENT OFFICE</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-2 bg-base-200">Gender</td>
+                                <td class="border px-2">Female</td>
+                                <td class="border px-2 bg-base-200">Position</td>
+                                <td class="border px-2">CITY GOVERNMENT DEPARTMENT HEAD I</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-2 bg-base-200">Status</td>
+                                <td class="border px-2">Permanent</td>
+                                <td class="border px-2 bg-base-200">Nature of Assignment</td>
+                                <td class="border px-2">Supervisory</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-2 bg-base-200">Category</td>
+                                <td class="border px-2">Key Position</td>
+                                <td class="border px-2 bg-base-200">Salary Grade</td>
+                                <td class="border px-2">25</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-2 bg-base-200">Level</td>
+                                <td class="border px-2">2</td>
+                                <td class="border px-2 bg-base-200">IPCR STARTS/STARTED IN:</td>
+                                <td class="border px-2"></td>
+                            </tr>
+                            <tr>
+                                <td class="border px-2 bg-base-200">Since</td>
+                                <td class="border px-2"></td>
+                                <td class="border px-2 bg-base-200"></td>
+                                <td class="border px-2"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    
+</template>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,14 +3,13 @@ import Navbar from '@/Components/Navbar.vue';
 </script>
 
 <template>
-    <div class="max-w-3xl h-screen">
-        <div class="absolute">
+    <div class="max-w-3xl">
+        <div class="fixed">
             <Navbar />
         </div>
         
         <div>
             <slot />
         </div>
-        
     </div>
 </template>
